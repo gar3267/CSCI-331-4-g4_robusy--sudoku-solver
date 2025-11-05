1c5b7a4752a0ee9f52d0969ef0aa9e02cc5e0e70
gar3267 <garettbearr101@gmail.com>
2025-11-05 10:22:49 -0500

    uncommented backtracking an dmoved it after forward checking

--- src/sudokuRunner.py
+++ src/sudokuRunner.py
@@ -77,19 +77,19 @@ def testSearchAlgorithms():
         count, seconds = sudokuSearch.backtrackPrunedSudokuTime(board[0])
         print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
 
-    # Testing with backtrack
-   # print('Testing with basic backtracking algorithm.')
-   # testBoards = getBoardsFromFolder()
-  #  for board in testBoards:
-   #     print('\nTesting '+str(board[1]))
-   #     count, seconds = sudokuSearch.backtrackSudokuTime(board[0])
-   #     print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
-
     # Testing with forward checking
     print('Testing with forward checking algorithm.')
     testBoards = getBoardsFromFolder()
     for board in testBoards:
         print('\nTesting ' + str(board[1]))
         count, seconds = sudokuSearch.forwardCheckingSudokuTime(board[0])
+        print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
+
+    # Testing with backtrack
+    print('Testing with basic backtracking algorithm.')
+    testBoards = getBoardsFromFolder()
+    for board in testBoards:
+        print('\nTesting '+str(board[1]))
+        count, seconds = sudokuSearch.backtrackSudokuTime(board[0])
         print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
 
