1b32a8130a7898d4f6c9d232f8ebd9895c2a879d
Veronika Kirievskaya <vak4215@rit.edu>
2025-11-22 21:04:28 -0500

    generating analysis

--- code/src/sudokuRunner.py
+++ code/src/sudokuRunner.py
@@ -1,6 +1,7 @@
 import src.sudokuSearch as sudokuSearch
 from src.sudokuBoard import Board
 import os
+import csv
 
 def testBoard(withUser=False):
     board0:Board = Board(lexicon=['a','b','c'],board=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']])
@@ -97,15 +98,119 @@ def testPruningAlgorithms():
     # Testing with pruned
 
     board1:Board
-    with open('src/sudoku_boards/board_16x16.sud','r') as file:
+    with open('src/sudoku_boards/board_4x4.sud','r') as file: # 16x16.sud','r') as file:
         board1 = Board(file=file)
 
-    print('\nSolving...\n')
+    print('\nSolving 4x4 Board with Pruning ...\n')
 
     count, seconds = sudokuSearch.backtrackPrunedSudokuTime(board1)
     print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
 
-    answer1:Board
+    """ answer1:Board # CSCI-331-6-Group-4_robust-sudoku-solver/code/src/sudoku_boards/answers/answer_4x4.sud
+    with open('src/sudoku_boards/answers/answer_4x4.sud','r') as file:
+        answer1 = Board(file=file)
+    print('\n'+str(board1.equals(answer1))) """
+
+
+    # Testing with pruned
+
+    board1:Board
+    with open('src/sudoku_boards/board2.sud','r') as file: # 16x16.sud','r') as file:
+        board1 = Board(file=file)
+
+    print('\nSolving 9x9 Board with Pruning ...\n')
+
+    count, seconds = sudokuSearch.backtrackPrunedSudokuTime(board1)
+    print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
+
+    """ answer1:Board
     with open('src/sudoku_boards/answers/answer1.sud','r') as file:
         answer1 = Board(file=file)
-    print('\n'+str(board1.equals(answer1)))
+    print('\n'+str(board1.equals(answer1))) """
+
+
+
+
+
+    # Testing with forward
+
+    board1:Board
+    with open('src/sudoku_boards/board_4x4.sud','r') as file: # 16x16.sud','r') as file:
+        board1 = Board(file=file)
+
+    print('\nSolving 4x4 Board with Forward Checking ...\n')
+
+    count, seconds = sudokuSearch.forwardCheckingSudokuTime(board1)
+    print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
+    print(board1)
+    """ 
+
+    answer1:Board
+    with open('src/sudoku_boards/answers/answer_4x4.sud','r') as file:
+        answer1 = Board(file=file)
+    print('\n'+str(board1.equals(answer1))) """
+
+
+    # Testing with forward
+
+    board1:Board
+    with open('src/sudoku_boards/board2.sud','r') as file: # 16x16.sud','r') as file:
+        board1 = Board(file=file)
+
+    print('\nSolving 9x9 Board with Forward Checking ...\n')
+
+    count, seconds = sudokuSearch.forwardCheckingSudokuTime(board1)
+    print('\nSolved in ' + str(seconds) + ' seconds with ' + str(count) + ' backtracking steps\n')
+    print(board1)
+
+    """ answer1:Board
+    with open('src/sudoku_boards/answers/answer1.sud','r') as file:
+        answer1 = Board(file=file)
+    print('\n'+str(board1.equals(answer1))) """
+
+    # run_experiments()
+
+    
+def run_experiments():
+    """
+    Run pruned backtracking and forward checking on a list of Sudoku boards
+    and save results (board name, algorithm, backtracks, runtime) to CSV.
+    """
+
+    output_csv='src/sudoku_boards/results.csv'
+    results = []
+    board_files=[
+        'src/sudoku_boards/board_4x4.sud',
+        'src/sudoku_boards/board_9x9_1.sud',
+        #'src/sudoku_boards/board_9x9_2.sud',
+        'src/sudoku_boards/board_9x9_3.sud',
+        'src/sudoku_boards/board_9x9_4.sud',
+        'src/sudoku_boards/board2.sud'
+        ]
+
+    for board_file in board_files:
+        # Load board
+        with open(board_file, 'r') as f:
+            board = Board(file=f)
+
+        board_name = board_file.split("/")[-1]  # just filename for readability
+
+        # --- Pruned Backtracking ---
+        count, seconds = sudokuSearch.backtrackPrunedSudokuTime(board)
+        results.append([board_name, "Pruned Backtracking", count, seconds])
+
+        # Reload board (since previous run modified it)
+        with open(board_file, 'r') as f:
+            board = Board(file=f)
+
+        # --- Forward Checking ---
+        count, seconds = sudokuSearch.forwardCheckingSudokuTime(board)
+        results.append([board_name, "Forward Checking", count, seconds])
+
+    # Write results to CSV
+    with open(output_csv, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Board", "Algorithm", "Backtracks", "Runtime (s)"])
+        writer.writerows(results)
+
+    print(f"Results written to {output_csv}")
